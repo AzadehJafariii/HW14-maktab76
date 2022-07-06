00ee8d4b2ca5b5ce6d8c8eb7b589e0c61c3c2045
AzadehJafariii <jafari.azadeh2018@gmail.com>
2022-07-06 23:30:44 +0430

    ./5/app.js

--- 5/app.js
+++ 5/app.js
@@ -1,5 +1,7 @@
-$("button").click(function () {
+
+
   $(document).ready(function () {
+    $("button").click(function () {
     $.ajax({
       url: "https://62c462c37d83a75e39f89aea.mockapi.io/api/users",
     }).done(function (data, status) {
